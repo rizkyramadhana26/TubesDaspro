898c2f96515737fe24a932bc4021c57903ce101e
Ishlah Muhammadi <81602663+slehmadi@users.noreply.github.com>
2021-04-24 22:54:29 +0800

    Update dashboard.py
    salah dikit

--- dashboard.py
+++ dashboard.py
@@ -71,7 +71,7 @@ def Show(role):    # Laman Utama [Ini tidak sesuai spesifikasi yang diinginkan]
             print('Anda tidak memiliki ijin akses')
             Show(role)
     if inUser == 'riwayatambil':
-        if role = 'admin':
+        if role == 'admin':
             consumable.riwayatambil()
         else:
             print('Anda tidak memiliki ijin akses')
